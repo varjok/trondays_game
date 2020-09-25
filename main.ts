371902acf79ada38f4e95a6b8f31778ec6511621
Varvara <71887939+varjok@users.noreply.github.com>
2020-09-25 17:05:09 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-function lights () {
-	
-}
 input.onGesture(Gesture.Shake, function () {
     makerController.player2.setButton(ArcadeButton.Down, true)
 })
 forever(function () {
-    while (!(input.switchRight())) {
+    while (input.switchRight()) {
         if (input.buttonA.isPressed()) {
             makerController.player1.setButton(ArcadeButton.A, true)
         } else if (input.buttonB.isPressed()) {
@@ -16,7 +13,7 @@ forever(function () {
             makerController.player1.setButton(ArcadeButton.B, false)
         }
     }
-    while (input.switchRight()) {
+    while (!(input.switchRight())) {
         makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
     }
 })
