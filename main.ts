47d7045ab0d6e2a46ac47ea7bae094b8a986cb6e
jocelym <43392541+jocelym@users.noreply.github.com>
2020-09-25 11:24:38 -0400

    added some lights

--- main.ts
+++ main.ts
@@ -2,12 +2,21 @@ input.onGesture(Gesture.Shake, function () {
     makerController.player2.setButton(ArcadeButton.Down, true)
 })
 music.powerUp.play()
+light.showRing(
+`green green green green green green green green green green`
+)
 forever(function () {
     while (input.switchRight()) {
         if (input.buttonA.isPressed()) {
             makerController.player1.setButton(ArcadeButton.A, true)
+            light.showRing(
+            `blue purple blue purple blue black black black black black`
+            )
         } else if (input.buttonB.isPressed()) {
             makerController.player1.setButton(ArcadeButton.B, true)
+            light.showRing(
+            `black black black black black pink white pink white pink`
+            )
         } else if (!(input.buttonA.isPressed())) {
             makerController.player1.setButton(ArcadeButton.A, false)
         } else if (!(input.buttonB.isPressed())) {
